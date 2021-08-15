c77a71a1ac7da3a90b0c7204074e867f4f617ab4
Swanav <swanav@outlook.com>
2021-08-15 13:56:09 +0530

    Added icons.

--- src/Header.svelte
+++ src/Header.svelte
@@ -5,25 +5,38 @@
     </div>
     <div>
         <div>
-            <div class="p-2 hover:bg-gray-50 cursor-pointer">
-                PPD-1202, Omaxe Palm Greens <br/>
-                Sector Mu, Greater Noida <br/>
-                Uttar Pradesh - 201310 <br/>
-            </div>
-            <div class="p-2 hover:bg-gray-50 cursor-pointer">
-                +91 9821880713
-            </div>
-            <div class="p-2 hover:bg-gray-50 cursor-pointer">
-                <a href="https://linkedin.com/in/swanav" target="_blank">
-                    <img id="linkedin-img" src={linkedInSvg} alt="linkedin"/>
-                    <!-- {@html LinkedIn} -->
-                    <span>/in/swanav</span>
+            <div class="p-2 hover:bg-gray-50 cursor-pointer icon-detail">
+                <a href="https://maps.apple.com/place?q=Omaxe%20Palm%20Greens&auid=10591049270420272818&address=MU,%20Greater%20Noida,%20201310,%20Uttar%20Pradesh,%20India&ll=28.477327,77.559706" target="_blank" >
+                    <img id="home-img" src={homeSvg} alt="home"/>
+                    <div>
+                        PPD-1202, Omaxe Palm Greens <br/>
+                        Sector Mu, Greater Noida <br/>
+                        Uttar Pradesh - 201310 <br/>
+                    </div>
                 </a>
             </div>
-            <div class="p-2 hover:bg-gray-50 cursor-pointer">
+            <div class="p-2 hover:bg-gray-50 cursor-pointer icon-detail">
+                <a href="tel:+919821880713">
+                    <img id="phone-img" src={phoneSvg} alt="phone"/>
+                    <div>
+                        <b>+91</b> 9821880713
+                    </div>
+                </a>
+            </div>
+            <div class="p-2 hover:bg-gray-50 cursor-pointer icon-detail">
+                <a href="https://linkedin.com/in/swanav" target="_blank">
+                    <img id="linkedin-img" src={linkedSvg} alt="linkedin"/>
+                    <div>
+                        <span>/in/swanav</span>
+                    </div>
+                </a>
+            </div>
+            <div class="p-2 hover:bg-gray-50 cursor-pointer icon-detail" >
                 <a href="https://github.com/swanav" target="_blank">
                     <img id="github-img" src={githubSvg} alt="github"/>
-                    <span>/swanav</span>
+                    <div>
+                        <span>/swanav</span>
+                    </div>
                 </a>
             </div>
         </div>
@@ -33,9 +46,6 @@
 
 <style>
 
-    #linkedin-img {
-        color: blue;
-    }
 
     img {
         max-height: 20px;
@@ -47,31 +57,49 @@
 
     header > div:first-child {
         color: #ff3e00;
-        font-weight: bold;
-        flex-grow: 2;
+        flex-grow: 1;
         vertical-align: middle;
         margin: auto;
+        padding-left: 20px;
     }
 
     header > div:last-child{
         /* flex-grow: 1; */
         align-content: right;
+        padding-right: 20px;
     }
 
     header > div:last-child > div {
         /* display: flex; */
+
     }
 
-    /* h3 {
-		color: #ff3e00;
-        font-weight: bold;
-	} */
+    .icon-detail > a {
+        display: flex;
+    }
+
+    .icon-detail > a > img {
+        /* flex-grow: 1; */
+        padding: 0px 10px;
+        margin: auto;
+    }
+
+    .icon-detail > a > div {
+        flex-grow: 1;
+    }
+
 </style>
 
 <script lang="ts">
     import Heading from "./components/Heading.svelte";
+
+    import Home from "./icons/home-solid.svg";
+    import Phone from "./icons/phone-square-solid.svg";
     import Github from "./icons/github-brands.svg";
-    import LinkedIn from "./icons/linkedin-in-brands.svg";
+    import LinkedIn from "./icons/linkedin-brands.svg";
+
+    const homeSvg = `data:image/svg+xml,${encodeURIComponent(Home)}`; 
+    const phoneSvg = `data:image/svg+xml,${encodeURIComponent(Phone)}`; 
     const githubSvg = `data:image/svg+xml,${encodeURIComponent(Github)}`; 
-    const linkedInSvg = `data:image/svg+xml,${encodeURIComponent(LinkedIn)}`; 
+    const linkedSvg = `data:image/svg+xml,${encodeURIComponent(LinkedIn)}`; 
 </script>
