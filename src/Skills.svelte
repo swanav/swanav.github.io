0443a57d490cd55e0f9e5ab875f0b5538cf7b45e
Swanav <swanav@outlook.com>
2021-08-15 13:57:14 +0530

    - Templating in skills - Improved Chips

--- src/Skills.svelte
+++ src/Skills.svelte
@@ -1,29 +1,70 @@
+<script lang="ts">
+    import Chip from "./components/Chip.svelte";
+    import Heading from "./components/Heading.svelte";
+
+    const skills = [
+        {
+            name: "C",
+            url: "https://en.wikipedia.org/wiki/C_(programming_language)",
+        },
+        {
+            name: "Dart",
+            url: "https://dart.dev",
+        },
+        {
+            name: "Java",
+            url: "https://www.oracle.com/in/java/",
+        },
+        {
+            name: "Javascript",
+            url: "https://www.javascript.com/",
+        },
+        {
+            name: "Typescript",
+            url: "https://www.typescriptlang.org/",
+        },
+        {
+            name: "Microsoft Azure IoTHub",
+            url: "https://azure.microsoft.com/en-in/services/iot-hub/",
+        },
+        {
+            name: "Google Firebase",
+            url: "https://firebase.google.com/",
+        },
+        {
+            name: "AWS Lambda",
+            url: "https://aws.amazon.com/lambda/",
+        },
+        {
+            name: "Espressif ESP32",
+            url: "https://www.espressif.com/en/products/socs/esp32",
+        },
+        {
+            name: "Nordic nRF52",
+            url: "https://www.nordicsemi.com/Products/Bluetooth-Low-Energy",
+        },
+        {
+            name: "Android",
+            url: "https://developer.android.com/",
+        },
+        {
+            name: "Flutter",
+            url: "https://flutter.dev/",
+        },
+    ];
+</script>
+
 <section>
     <Heading size="text-2xl">Skills</Heading>
     <div>
-        <!-- Programming Languages -->
-        <Chip>C</Chip>
-        <Chip>Dart</Chip>
-        <Chip>Java</Chip>
-        <Chip>Javascript</Chip>
-        <Chip>Typescript</Chip>
-        <!-- Cloud Services -->
-        <Chip>Microsoft Azure IoTHub</Chip>
-        <Chip>Google Firebase</Chip>
-        <Chip>AWS Lambda</Chip>
-        <!-- Hardware Platforms -->
-        <Chip>Espressif ESP32</Chip>
-        <Chip>Nordic nRF52</Chip>
+        {#each skills as skill}
+            <Chip link={skill.url}>{skill.name}</Chip>
+        {/each}
     </div>
 </section>
 
 <style>
-section {
-    padding: 10px 20px;
-}
+    section {
+        padding: 10px 20px;
+    }
 </style>
-
-<script lang="ts">
-import Chip from "./components/Chip.svelte"
-import Heading from "./components/Heading.svelte";
-</script>
